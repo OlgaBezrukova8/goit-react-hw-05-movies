f91ddd47c75bfd2f69c8967c04161822dfa70580
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-24 15:51:32 +0100

    Added styles to Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,23 @@
 import { NavLink } from 'react-router-dom';
+import styles from '../Navigation/Navigation.module.css';
+
+const getClassName = ({ isActive }) => {
+  const className = isActive ? `${styles.link} ${styles.active}` : styles.link;
+  return className;
+};
 
 export const Navigation = () => {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
+    <ul className={styles.list}>
+      <li className={styles.item}>
+        <NavLink className={getClassName} to="/">
+          Home
+        </NavLink>
       </li>
-      <li>
-        <NavLink to="/movies">Movies</NavLink>
+      <li className={styles.item}>
+        <NavLink className={getClassName} to="/movies">
+          Movies
+        </NavLink>
       </li>
     </ul>
   );
