42815a7fbbb86efa0f17f4c67d1b833c92a1013b
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-24 17:22:51 +0100

    Added content to NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,1 +1,13 @@
-export const NotFoundPage = () => {};
+import { Link } from 'react-router-dom';
+
+export const NotFoundPage = () => {
+  return (
+    <div>
+      <h2>Ooops, page not found.</h2>
+      <p>
+        Please, go back to the
+        <Link to="/">Home</Link> page :)
+      </p>
+    </div>
+  );
+};
