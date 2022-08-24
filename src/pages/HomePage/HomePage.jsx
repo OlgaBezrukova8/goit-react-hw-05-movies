ef5bba53af608c698a21c351343bc1b3d664a195
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-24 17:13:12 +0100

    Added tranding movies to the home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,1 +1,43 @@
-export const HomePage = () => {};
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchApiTrendingMovies } from '../../servises/fetchApiMovies';
+// import { Loader } from '../../components/Loader/Loader';
+
+export const HomePage = () => {
+  const [movies, setMovies] = useState(null);
+  //   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    fetchApiTrendingMovies()
+      .then(setMovies)
+      .catch(error => {
+        setError({ error });
+      });
+  }, []);
+
+  return (
+    <>
+      {/* {isLoading && <Loader />} */}
+      {error &&
+        Notify.failure(
+          `Ooops, something happend wrong ${
+            error.message
+          }. Please, return to the ${(<Link to="/">Home page</Link>)}`
+        )}
+      <ul>
+        {movies &&
+          movies.results.map(({ title, id }) => (
+            <li key={id}>
+              <Link state={{ from: location.pathname }} to={id}>
+                {title}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
