2ece69bc3045877216ce5ad1598e9d49eccc6d05
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-27 17:52:05 +0100

    Added logic search form to MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,10 +1,52 @@
-// import { useState } from 'react';
-// import { SearchForm } from '../../components/SearchForm/SearchForm';
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchApiSearchMovies } from '../../servises/fetchApiMovies';
+import { SearchForm } from '../../components/SearchForm/SearchForm';
 
 const MoviesPage = () => {
-  //   const [search, setSearch] = useState('');
-  //   const [error, setError] = useState(null);
-  //   return <SearchForm />;
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const search = searchParams.get('search');
+
+  const location = useLocation();
+
+  const onChangeSearch = ({ search }) => {
+    setSearchParams({ search });
+  };
+
+  useEffect(() => {
+    if (search) {
+      fetchApiSearchMovies(search)
+        .then(setMovies)
+        .catch(error => {
+          setError({ error });
+        });
+    }
+  }, [search]);
+
+  return (
+    <>
+      <SearchForm onSubmit={onChangeSearch} />
+      {error &&
+        Notify.failure(
+          `Ooops, something went wrong ${error.message}. Please, reload the page`
+        )}
+      {movies.length > 0 && (
+        <ul>
+          {movies.map(({ id, title }) => (
+            <li key={id}>
+              <Link state={{ from: location }} to={`/movies/${id}`}>
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default MoviesPage;
