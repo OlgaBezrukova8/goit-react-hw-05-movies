db163cb25a1950fb2b107e6d5e6de789db065f28
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-28 10:07:11 +0100

    Added styles in Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,19 @@
 import { ThreeDots } from 'react-loader-spinner';
+import style from '../Loader/Loader.module.css';
 
 export const Loader = () => {
   return (
-    <ThreeDots
-      height="80"
-      width="80"
-      radius="9"
-      color="#008b8b"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
+    <div className={style.loader}>
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#008b8b"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </div>
   );
 };
