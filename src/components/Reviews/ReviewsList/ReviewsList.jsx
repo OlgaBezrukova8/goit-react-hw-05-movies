91f1f2f9af6335673ce6dee54cd453454b7710da
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-29 11:16:14 +0100

    Added styles to ReviewPage

--- src/components/Reviews/ReviewsList/ReviewsList.jsx
+++ src/components/Reviews/ReviewsList/ReviewsList.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import styles from '../ReviewsList/ReviewList.module.css';
 
 export const ReviewsList = ({ array }) => {
   return array.map(({ id, author, content }) => (
-    <li key={id}>
-      <p>{author}</p>
-      <p>{content}</p>
+    <li className={styles.item} key={id}>
+      <p className={styles.author}>@{author}</p>
+      <p className={styles.content}>{content}</p>
     </li>
   ));
 };
--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -1,14 +1,15 @@
 import { useFetchMoviesReviews } from '../../hooks/useFetchMoviesReviews';
 import { ReviewsList } from '../../components/Reviews/ReviewsList/ReviewsList';
 import { Loader } from '../../components/Loader/Loader';
+import styles from '../ReviewsPage/ReviewsPage.module.css';
 
 const ReviewsPage = () => {
   const reviews = useFetchMoviesReviews();
   const noInfoMessage = "We don't have any reviews for this movie.";
 
   return (
-    <div>
-      <ul>
+    <div className={styles.box}>
+      <ul className={styles.list}>
         {reviews === null && <Loader />}
         {reviews !== null && reviews.length < 0 && noInfoMessage}
         {reviews !== null && reviews.length > 0 && (
